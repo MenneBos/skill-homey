331d0767da6c999f2b44339f8a592e81ffb33c39
MenneBos <mennebos@gmail.com>
2023-07-29 21:55:35 +0200

    Update Homey.py

--- Homey.py
+++ Homey.py
@@ -47,10 +47,10 @@ class Homey:
         #output => [nodename, stype, properties] or None if nothing found
         #===START===>
         if self.lang1 == 'nl-nl':
+	    print("self.lang1== nl-nl")	
             if what[-2:] == "en" : what = what[:len(what)-2]
             if what[-1:] == "s": what = what[:len(what) - 1]
         else :what = what[:len(what)-1]
-	print(what)
         wht = re.compile(what, re.I)
         result = []
         devices = self.ha.getdevicesjson()
