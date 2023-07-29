81260b99e855a9da89383425132044044f87c23d
MenneBos <mennebos@gmail.com>
2023-07-29 02:03:10 +0200

    Update Homey.py

--- Homey.py
+++ Homey.py
@@ -23,7 +23,7 @@ class Homey:
         #===START===>
         wht = re.compile(what, re.I)
         whr = re.compile(where, re.I)
-	print("Menne testdata:",wht,whr)
+	print('Menne testdata:',wht,whr)
         result = []
         devices = self.ha.getdevicesjson()
         i=0
