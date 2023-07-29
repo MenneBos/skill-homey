85cb4524575b4d3be626eec364d64f7828026779
MenneBos <mennebos@gmail.com>
2023-07-29 21:19:08 +0200

    Update Homey.py

--- Homey.py
+++ Homey.py
@@ -26,14 +26,12 @@ class Homey:
         whr = re.compile(where, re.I)
         result = []
         devices = self.ha.getdevicesjson()
-	#print(devices)
         i=0
         while i < len(devices['Devices'][0]['Nodes']):
             if whr.search(devices['Devices'][0]['Nodes'][i]['Name']) or wht.search(devices['Devices'][0]['Nodes'][i]['Name']):
                 sname = devices['Devices'][0]['Nodes'][i]['Name']
-		#print('Menne sname:', sname)
+		print(sname)
                 stype = devices['Devices'][0]['Nodes'][i]['Type']
-		#print('Menne sname:' + stype)
                 typ = re.compile(stype, re.I)
                 snode_id = devices['Devices'][0]['Nodes'][i]['Node_id']
                 sproperties ={}
