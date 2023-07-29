b94ff1970cc11730c689a5fe899b2766dfa31a21
MenneBos <mennebos@gmail.com>
2023-07-29 23:34:38 +0200

    Update Homey.py

--- Homey.py
+++ Homey.py
@@ -65,7 +65,7 @@ class Homey:
                 sproperties ={}
                 for property in devices['Devices'][0]['Nodes'][i]['Properties']:
                     sproperties[property['Name']]=property['Value']
-                result.append([snode_id,sname,typ,sproperties])
+                result.append([snode_id,sname,stype,sproperties])
             i += 1
         return result
 
