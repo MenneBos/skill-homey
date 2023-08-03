e214f81bded40a4c70ae60b34a2e7f4e3e9d5b93
MenneBos <mennebos@gmail.com>
2023-08-04 01:47:49 +0200

    Update __init__old.py

--- __init__old.py
+++ __init__old.py
@@ -14,7 +14,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
-
 #from adapt.intent import IntentBuilder
 #from mycroft.skills.core import MycroftSkill
 #from mycroft.util.log import getLogger
